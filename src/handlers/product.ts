6cada7125b5b64d9aa20028014fc7d744bd83a2f
Jared Garcia <jaredgarcia@MacBook-Pro-de-Jared.local>
2024-05-21 19:47:22 -0600

    Middlewares

--- src/handlers/product.ts
+++ src/handlers/product.ts
@@ -1,26 +1,41 @@
 import { Request, Response } from "express";
-import { check, validationResult } from "express-validator";
+
 import Product from "../models/Product.model";
 
-export const createProduct = async (req: Request, res: Response) => {
-  await check("name")
-    .notEmpty()
-    .withMessage("El nombre del Producto no puede ir vacio")
-    .run(req);
-  await check("price")
-    .isNumeric()
-    .withMessage("Valor no Válido")
-    .notEmpty()
-    .withMessage("El prceio del Producto no puede ir vacio")
-    .custom((value) => value > 0)
-    .withMessage("Precio no válido")
-    .run(req);
+export const getProducts = async (req: Request, res: Response) => {
+  const products = await Product.findAll({
+    attributes: { exclude: ["createdAt", "updatedAt"] },
+  });
 
-  let errors = validationResult(req);
-  if (!errors.isEmpty()) {
-    return res.status(400).json({ errors: errors.array() });
+  res.json({ data: products });
+};
+export const getProductById = async (req: Request, res: Response) => {
+  try {
+    const { id } = req.params;
+    const product = await Product.findByPk(id);
+    if (!product) {
+      return res.status(404).json({ error: "Producto no Encontrado" });
+    }
+    res.json({ data: product });
+  } catch (error) {
+    console.log(error);
   }
-
-  const product = await Product.create(req.body);
+};
+export const createProduct = async (req: Request, res: Response) => {
+  try {
+    const product = await Product.create(req.body);
+    res.json({ data: product });
+  } catch (error) {
+    console.log(error);
+  }
+};
+export const updateProduct = async (req: Request, res: Response) => {
+  const { id } = req.params;
+  const product = await Product.findByPk(id);
+  if (!product) {
+    return res.status(404).json({ error: "Producto no Encontrado" });
+  }
+  await product.update(req.body);
+  await product.save();
   res.json({ data: product });
 };
--- src/router.ts
+++ src/router.ts
@@ -1,17 +1,61 @@
 import { Router } from "express";
-import { createProduct } from "./handlers/product";
+import {
+  createProduct,
+  getProductById,
+  getProducts,
+  updateProduct,
+} from "./handlers/product";
+import { body, param } from "express-validator";
+import { handleInputErrors } from "./middleware";
 
 const router = Router();
 //Routing
-router.get("/", (req, res) => {
-  res.send("Hola Mundo desde get");
-});
-router.post("/", createProduct);
+router.get("/", getProducts);
+router.get(
+  "/:id",
+  param("id").isInt().withMessage("ID no Válido"),
+  handleInputErrors,
+  getProductById
+);
+router.post(
+  "/",
+
+  body("name")
+    .notEmpty()
+    .withMessage("El nombre del Producto no puede ir vacio"),
+
+  body("price")
+    .isNumeric()
+    .withMessage("Valor no Válido")
+    .notEmpty()
+    .withMessage("El prceio del Producto no puede ir vacio")
+    .custom((value) => value > 0)
+    .withMessage("Precio no válido"),
+  handleInputErrors,
+  createProduct
+);
+router.put(
+  "/:id",
+  param("id").isInt().withMessage("ID no Válido"),
+  body("name")
+    .notEmpty()
+    .withMessage("El nombre del Producto no puede ir vacio"),
+
+  body("price")
+    .isNumeric()
+    .withMessage("Valor no Válido")
+    .notEmpty()
+    .withMessage("El prceio del Producto no puede ir vacio")
+    .custom((value) => value > 0)
+    .withMessage("Precio no válido"),
+  body("availability")
+    .isBoolean()
+    .withMessage("Valor para disponibilidad no válido"),
+  handleInputErrors,
+  updateProduct
+);
 router.delete("/", (req, res) => {
   res.send("Hola Mundo delete");
-});
-router.put("/", (req, res) => {
-  res.send("Hola Mundo put");
 });
 
 export default router;
